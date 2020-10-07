6db53f2255fadbf571de3d9fdcf04b78d7050f3b
Mike Skalandunas <mskalandunas@gmail.com>
2020-10-06 23:10:56 -0700

    create dev/prod scripts

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,6 +1,27 @@
 import gulp from 'gulp';
+import htmlmin from 'gulp-htmlmin';
+import include from 'gulp-include';
 import rename from 'gulp-rename';
 import sass from 'gulp-sass';
+
+gulp.task('html', () => gulp.src('./client/index.html')
+    .pipe(include())
+    .on('error', console.error)
+    .pipe(gulp.dest('./dist'))
+);
+
+gulp.task('html:optimized', () => gulp.src('./client/index.html')
+    .pipe(include())
+    .on('error', console.error)
+    .pipe(htmlmin({
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true,
+        removeComments: true,
+        removeEmptyAttributes: true
+    }))
+    .pipe(gulp.dest('./dist'))
+);
 
 gulp.task(
   'sass',
@@ -11,6 +32,22 @@ gulp.task(
     .pipe(gulp.dest('./dist'))
 );
 
-gulp.task('default', gulp.series([
+gulp.task('build:dev', gulp.series([
+  'html',
   'sass'
 ]));
+
+gulp.task('build:prod', gulp.series([
+  'html:optimized',
+  'sass'
+]));
+
+gulp.task('watch:dev', () => {
+  gulp.watch('./client/**/*.html', gulp.series(['html']));
+  gulp.watch('./client/**/*.scss', gulp.series(['sass']));
+});
+
+gulp.task('watch:prod', () => {
+  gulp.watch('./client/**/*.html', gulp.series(['html:optimized']));
+  gulp.watch('./client/**/*.scss', gulp.series(['sass']));
+});
